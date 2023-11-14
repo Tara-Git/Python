d0035567163043426e3aa20b25edfe9fd7c73303
Tara <behtoueiTar@sbsit.com>
2023-11-14 17:32:50 +0100

    added other projects

--- FirstPro/solution-us-states-game/main_weather.py
+++ FirstPro/solution-us-states-game/main_weather.py
@@ -10,7 +10,7 @@ import pandas
 #     print(temperatures)
 
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
 # print(type(data))
 # print(type(data['temp']))
 # data_dic = data.to_dict()
@@ -24,15 +24,28 @@ data = pandas.read_csv("weather_data.csv")
 # print(sum(temp_list) / len(temp_list))
 # print(data.condition)
 
-max_temp = data.temp.max()
-print(data[data.temp == max_temp])
+# max_temp = data.temp.max()
+# print(data[data.temp == max_temp])
+#
+# monday = data[data.day == 'Monday']
+# print(monday.temp * 9/5 + 32)
+# dat_dict = {
+#     'students': ['amy', 'James', 'Angela'],
+#     'scores': [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(dat_dict)
+# print(data)
 
-monday = data[data.day == 'Monday']
-print(monday.temp * 9/5 + 32)
-dat_dict = {
-    'students': ['amy', 'James', 'Angela'],
-    'scores': [76, 56, 65]
+data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
+gray = data[data['Primary Fur Color'] == 'Gray']
+cinnamon = data[data['Primary Fur Color'] == 'Cinnamon']
+white = data[data['Primary Fur Color'] == 'White']
+black = data[data['Primary Fur Color'] == 'Black']
+
+new_dict = {
+    'Color': ['Gray', 'Cinnamon', 'White', 'Black'],
+    'Count': [len(gray), len(cinnamon), len(white), len(black)]
 }
-
-data = pandas.DataFrame(dat_dict)
-print(data)
+df = pandas.DataFrame(new_dict)
+df.to_csv("squirrel_count.csv")
